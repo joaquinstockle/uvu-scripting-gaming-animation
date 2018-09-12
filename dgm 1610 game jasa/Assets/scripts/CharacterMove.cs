a582855450f7432bb3242b24a6f35a5495e19071
joaquinstockle <joaquinstockle@live.com>
2018-09-12 17:23:02 -0600

    Added the jumping mechanic with code

--- dgm 1610 game jasa/Assets/scripts/CharacterMove.cs
+++ dgm 1610 game jasa/Assets/scripts/CharacterMove.cs
@@ -7,13 +7,30 @@ public class CharacterMove : MonoBehaviour {
 	public int MoveSpeed; 
 	public float JumpHeight;
 
+	// Player Grounded Variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
+
 	// Use this for initialization
 	void Start () {
 		print("Hello World");
 	}
 	
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.posotion, GroundCheckRadius, WhatIsGround);
+	}
 	// Update is called once per frame
 	void Update () {
 		
+		// This makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+		}
+	}
+
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity + new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }

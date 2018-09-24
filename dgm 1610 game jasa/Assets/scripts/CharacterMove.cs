0bc343026c13d2505bf0fc171c9504f810626ca7
joaquinstockle <joaquinstockle@live.com>
2018-09-24 17:19:45 -0600

    Finished the jump/ double jump started scoring

--- dgm 1610 game jasa/Assets/scripts/CharacterMove.cs
+++ dgm 1610 game jasa/Assets/scripts/CharacterMove.cs
@@ -6,12 +6,16 @@ public class CharacterMove : MonoBehaviour {
 
 	public int MoveSpeed; 
 	public float JumpHeight;
+	private bool DoubleJump;
 
 	// Player Grounded Variables
 	public Transform GroundCheck;
 	public float GroundCheckRadius;
 	public LayerMask WhatIsGround;
 	private bool Grounded;
+
+	// Non Stick Player
+	private float MoveVelocity;
 
 	// Use this for initialization
 	void Start () {
@@ -28,14 +32,31 @@ public class CharacterMove : MonoBehaviour {
 		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
 			Jump();
 		}
-		// This cod makes the character move from side to side using the A&D keys.
-		if(Input.GetKey (KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		//Double Jump Code
+		if(Grounded)
+			DoubleJump = false;
+
+		if(Input.GetKeyDown (KeyCode.Space)&& !DoubleJump && !Grounded){
+			Jump();
+			DoubleJump = true;
 		}
 
+		//Non Stick Player
+		MoveVelocity = 0f;
+		
+		// This cod makes the character move from side to side using the A&D keys.
+		if(Input.GetKey (KeyCode.D)){
+			// 	GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = MoveSpeed;
+		 }
+
 		if(Input.GetKey (KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = -MoveSpeed;
 		}
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 	}
 
 	public void Jump(){

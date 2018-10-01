5406c84ce47573df3ad75c2096d1b1a06a3e9de6
joaquinstockle <joaquinstockle@live.com>
2018-10-01 17:10:27 -0600

    added point system and coin collection

---
+++ dgm 1610 game jasa/Assets/scripts/CoinCollect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour {
+
+	public int PointToAdd;
+
+	void OnTriggerEnter2D (Collider2D Other) {
+		if (Other.GetComponent<Rigidbody2D> () == null)
+			return;
+
+		ScoreManager.Addpoints (PointToAdd);
+
+		Destroy (gameObject);
+	}
+}

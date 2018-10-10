53acc782467197b0e8e38ed093a691659de86140
joaquinstockle <joaquinstockle@live.com>
2018-10-10 16:13:14 -0600

    made ancestry

---
+++ dgm 1610 game jasa/Assets/scripts/assign/fruit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruit : food {
+
+	public bool Sweet;
+	public int Size;
+	public Color SkinColor;
+}

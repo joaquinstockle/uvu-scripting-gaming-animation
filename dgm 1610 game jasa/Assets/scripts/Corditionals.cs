44c86838dbe6db36ae8b57f0e011719a2553f244
joaquinstockle <joaquinstockle@live.com>
2018-10-10 16:31:32 -0600

    Ifs

---
+++ dgm 1610 game jasa/Assets/scripts/Corditionals.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corditionals : MonoBehaviour {
+
+	public string Weather;
+
+
+
+	// Use this for initialization
+	void Start () {
+		if(Weather == "Raining")
+			print("Weather is " + Weather);
+		else if(Weather == "Snowing")
+			print("Weather is " + Weather);
+		else if(Weather == "Cloudy")
+			print("Weather is " + Weather);
+		else
+			print("Weather is Clear");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

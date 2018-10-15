8ebb59b653ded315a73d58895ced04acce30fef8
joaquinstockle <joaquinstockle@live.com>
2018-10-15 17:25:05 -0600

    Added Enemy Patrol/Enemy

---
+++ dgm 1610 game jasa/Assets/scripts/EnemyPatrol.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour {
+
+	//movement vairiables
+	public float MoveSpeed;
+	public bool MoveRight;
+
+	//Wall check
+	public Transform WallCheck;
+	public float WallCheckRadius;
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+
+	//Edge Check
+	private bool NotAtEdge;
+	public Transform EdgeCheck;
+
+
+	// Use this for initialization
+	void Start () {
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+		
+		//Turn Around At Edge
+		if(HittingWall || !NotAtEdge){
+			MoveRight = !MoveRight;
+		}
+
+		if(MoveRight){
+			transform.localScale = new Vector3(-9f,9f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else {
+			transform.localScale = new Vector3(9f,9f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ dgm 1610 game jasa/Assets/scripts/assign/Time.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time : MonoBehaviour {
+
+	public string Abc;
+
+	// Use this for initialization
+	void Start () {
+
+		if(Abc == "party")
+			print("It is " + Abc + " time.");
+		// else(Abc == "sleep")
+		// 	print("It is " + Abc + " time.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

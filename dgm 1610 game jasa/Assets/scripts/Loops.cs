387d88d4ea63c3b99dd8231a6fba12ba02bde604
joaquinstockle <joaquinstockle@live.com>
2018-10-24 17:10:56 -0600

    Added Loops Brother

---
+++ dgm 1610 game jasa/Assets/scripts/Loops.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	public int Num = 40;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		while(Num > 0){
+			print("Countdown" + Num);
+			Num --;
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

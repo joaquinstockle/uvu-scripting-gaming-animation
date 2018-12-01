88eafeeee95323ce79a1ea043a42ee29a045e493
joaquinstockle <joaquinstockle@live.com>
2018-12-01 13:46:02 -0700

    Added Damage and Health Aid

---
+++ dgm 1610 game jasa/Assets/scripts/HealthPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour {
+
+	public int PointToAdd;
+
+	void OnTriggerEnter2D (Collider2D Other) {
+		if (Other.GetComponent<Rigidbody2D> () == null)
+			return;
+
+		HealthManager.Addpoints (PointToAdd);
+
+		Destroy (gameObject);
+	}
+}

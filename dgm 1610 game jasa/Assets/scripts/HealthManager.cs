0ac27a9c0712b5c77784312a3c7a49038b561638
joaquinstockle <joaquinstockle@live.com>
2018-12-01 13:10:11 -0700

    Added Health Manager

---
+++ dgm 1610 game jasa/Assets/scripts/HealthManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour {
+
+	public static int Health;
+
+	Text HealthText;
+
+	// Use this for initialization
+	void Start () {
+		HealthText = GetComponent<Text>();
+
+		Health = 100;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Health < 0)
+			Health = 0;
+		if (Health > 100)
+			Health = 100;
+		HealthText.text = " " + Health;	
+
+	}
+
+	public static void Addpoints (int PointToAdd) {
+		Health += PointToAdd;
+	}
+}

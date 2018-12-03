e98be5f1a359653ca0f510cf084b2a4952187e82
joaquinstockle <joaquinstockle@live.com>
2018-12-03 16:52:25 -0700

    Added Animation

---
+++ dgm 1610 game jasa/Assets/scripts/NewCheckpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCheckpoint : MonoBehaviour {
+
+	public LevelManager CurrentCheckpoint;
+	public GameObject PC;
+
+	// void OnTriggerEnter2D (Collider2D other){
+
+	// 	if(other.tag == "PC")
+	// 		LevelManager.CurrentCheckpoint();
+	// }
+
+}
